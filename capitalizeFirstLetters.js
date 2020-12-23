7375d06352bbd3e80befc657391540c85fb32e70
cleroy117 <ce.leroy69@gmail.com>
2020-12-23 18:12:54 +0100

    quest-2

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -14,15 +14,4 @@ function capitalizeFirstLetters(input) {
   return output;
 }
 
-assert.strictEqual(typeof capitalizeFirstLetters, "function");
-
-assert.strictEqual(capitalizeFirstLetters.length, 1);
-
-assert.strictEqual(capitalizeFirstLetters("javascript"), "Javascript");
-
-assert.strictEqual(
-  capitalizeFirstLetters("javascript roule sur PHP"),
-  "Javascript Roule Sur PHP"
-);
-
-assert.strictEqual(capitalizeFirstLetters(""), "");
+module.exports = capitalizeFirstLetters;
---
+++ test/capitalizeFirstLetters.test.js
@@ -0,0 +1,28 @@
+const assert = require("assert");
+const capitalizeFirstLetters = require("../capitalizeFirstLetters");
+
+describe("capitalizeFirstLetters", () => {
+  it("is a function accepting one argument", () => {
+    assert.strictEqual(typeof capitalizeFirstLetters, "function");
+    assert.strictEqual(capitalizeFirstLetters.length, 1);
+  });
+
+  it("transforms javaScript correctly", () => {
+    assert.strictEqual(capitalizeFirstLetters("javascript"), "Javascript");
+  });
+
+  it("works with a 1-character string", () => {
+    assert.strictEqual(capitalizeFirstLetters("c"), "C");
+  });
+
+  it("works with a string with several spaces", () => {
+    assert.strictEqual(
+      capitalizeFirstLetters("javascript roule sur PHP"),
+      "Javascript Roule Sur PHP"
+    );
+  });
+
+  it("works with a empty string", () => {
+    assert.strictEqual(capitalizeFirstLetters(""), "");
+  });
+});
