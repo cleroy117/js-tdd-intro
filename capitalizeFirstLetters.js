0a7e7f0ce97955ca305535da77e32a26754bce41
cleroy117 <ce.leroy69@gmail.com>
2020-12-23 14:29:32 +0100

    test OK

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,36 @@
 const assert = require("assert");
 
+/* function capitalizeFirstLetters(input) {
+  let result = "";
+  for (let i = 0; i < input.length; i++) {
+    if ((i = 0)) {
+      result += input[0].toUppercase();
+      wq;
+    } else if (input[i - 1] === " ") {
+      result += input[i].toUppercase();
+    } else {
+      result += input[i];
+    }
+  }
+  return result;
+} */
+
+function capitalizeFirstLetters(input) {
+  let output = "";
+  for (let i = 0; i < input.length; i++) {
+    if (i === 0) {
+      output += input[0].toUpperCase();
+    } else if (input[i - 1] === " ") {
+      output += input[i].toUpperCase();
+    } else {
+      output += input[i];
+    }
+  }
+  return output;
+}
+
+/* console.log(capitalizeFirstLetters("coucou")); */
+
 assert.strictEqual(typeof capitalizeFirstLetters, "function");
 
 assert.strictEqual(capitalizeFirstLetters.length, 1);
